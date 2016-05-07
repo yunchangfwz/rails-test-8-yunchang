068d28dadadfee322b6a5954f86b4dd0fc693db0
yunchang <yunchang@futureworkz.com>
2016-05-07 09:00:49 +0700

    Shoe#validates

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -1,2 +1,9 @@
 class Shoe < ActiveRecord::Base
+  
+  validates :name,         presence: true 
+  validates :sku,          presence: true
+  validates :price,        presence: true, numericality: { greater_than: 0 } 
+
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
