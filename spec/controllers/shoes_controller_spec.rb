bcc785ef9440542760ae8f54be95fa8e3063b732
yunchang <yunchang@futureworkz.com>
2016-05-07 15:57:46 +0700

    Add testcase for Search#order_by

--- spec/controllers/shoes_controller_spec.rb
+++ spec/controllers/shoes_controller_spec.rb
@@ -6,18 +6,21 @@ describe ShoesController, type: :controller do
       get :filter, search_filter: search_params, format: :js
     end
 
-    let!(:nice)          { create(:brand) }
-    let!(:puma)          { create(:brand) }
-    let!(:nice_shoe)     { create(:shoe, price: 10, brand: nice)}
-    let!(:puma_shoe)     { create(:shoe, brand: puma)}
+    let!(:nice)              { create(:brand) }
+    let!(:puma)              { create(:brand) }
+    let!(:nice_shoe)         { create(:shoe, name: 'Nice', price: 10, brand: nice)}
+    let!(:nice_beauty_shoe)  { create(:shoe, name: 'Beauty', price: 10, brand: nice)}
+    let!(:puma_shoe)         { create(:shoe, brand: puma)}
 
-    let!(:search_params) { { :brand_ids => [nice.id], min_price: 0, max_price: 20} }
+    let!(:search_params) { { :brand_ids => [nice.id], min_price: 0, max_price: 20, order_by: 'asc'} }
 
     it 'return result' do
       do_request
 
-      expect(assigns(:shoes).size).to eq 1
+      expect(assigns(:shoes).size).to eq 2
       expect(assigns(:shoes)).to include nice_shoe
+      expect(assigns(:shoes)).to include nice_beauty_shoe
+      expect(assigns(:shoes).first).to match nice_beauty_shoe
       expect(assigns(:shoes)).not_to include puma_shoe
     end
   end
